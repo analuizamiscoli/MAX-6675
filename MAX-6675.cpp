c97899a8ec8fdfae057a8a95350dd3664c31c5f4
Ana Luíza Míscoli <135639141+analuizamiscoli@users.noreply.github.com>
2023-10-11 11:59:55 -0300

    HX711+PWM.cpp

--- MAX-6675.cpp
+++ MAX-6675.cpp
@@ -2,20 +2,23 @@ void taskReadSensor(void *pvParameters);
 void taskWriteSD(void *pvParameters);
 void taskReadTermopar(void *pvParameters);
 
+#include <Arduino.h>
+#include "driver/ledc.h"
 #include <HX711.h>
 #include <SD.h>
 #include "max6675.h"
+
+
+#define PWM_PIN 2 // ALTERAR O PINO
+//#define PWM_CHANNEL 0 - Canal LEDC (VERIFICAR SE PRECISA)
 
 #define pinSO  19 
 #define pinCS  16  
 #define pinCLK 18  
 
+
 const int LOADCELL_DOUT_PIN = 2;
 const int LOADCELL_SCK_PIN = 3;
-const int PWM_PIN = 32;
-const int PWM_FREQ = 20000000;
-const int PWM_CHANNEL = 1;
-const int PWM_RESOLUTION = 8;
 
 MAX6675 sensorTemp(pinCLK, pinCS, pinSO); 
 HX711 scl;
@@ -31,14 +34,15 @@ void IRAM_ATTR isr() {
 }
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(PWM_PIN, OUTPUT);
-  ledcSetup(PWM_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
-  ledcAttachPin(PWM_PIN, PWM_CHANNEL);
+  Serial.begin(9600); // 115200
+  ledcSetup(0, 20000000 , 1);
+  ledcAttachPin(PWM_PIN, 0);
+  ledcWrite(0, 1);
   scl.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scl.set_scale(100.0);
   scl.tare();
   SD.begin();
+
   xsmfr = xSemaphoreCreateBinary();
   xTaskCreatePinnedToCore(
       taskReadSensor,
@@ -74,11 +78,10 @@ void taskReadSensor(void *pvParameters) {
   (void)pvParameters;
   while (true) {
     if (xSemaphoreTake(xsmfr, portMAX_DELAY) == pdTRUE) {
-      ledcWrite(PWM_CHANNEL, 255);
-      delayMicroseconds(25);
-      ledcWrite(PWM_CHANNEL, 0);
       brt = scl.read();
       liquido = brt - scl.get_offset();
+      int pwmValue = map(brt, 0, 1023, 0, 255); // Mapear a leitura do sensor para o ciclo de trabalho do PWM
+      ledcWrite(0, pwmValue);  // Atualizar o PWM com base na leitura do sensor
       xTaskNotifyGive(xSD);
     }
   }
@@ -99,7 +102,7 @@ void taskWriteSD(void *pvParameters) {
   }
 }
 
-void taskReadTermopar(coid *pvParameters) {
+void taskReadTermopar(void *pvParameters) {
   (void)pvParameters;
   while (true) {
     if (xSemaphoreTake(xsmfr, portMAX_DELAY) == pdTRUE) { 
